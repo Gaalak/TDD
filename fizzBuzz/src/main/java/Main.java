f9be86db9e5b827235236d032e614d2a7e74d5cf
Yoann <yoann.gralak@gmail.com>
2022-04-06 15:00:54 +0200

    commentaire

--- fizzBuzz/src/main/java/Main.java
+++ fizzBuzz/src/main/java/Main.java
@@ -3,12 +3,11 @@ import java.util.stream.IntStream;
 public class Main {
 
     public static void main(String[] args){
+        //IntStream.range(0,101).mapToObj(Main::fizzBuzz).forEach(System.out::println);
         System.out.println("Hello world!");
         for (int i = 0; i < 101; i++) {
             System.out.println(fizzBuzz(i));
         }
-
-        IntStream.range(0,101).mapToObj(Main::fizzBuzz).forEach(System.out::println);
     }
 
     public static String fizzBuzz (Integer nb){
