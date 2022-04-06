5d7c9bc157cb71db12e5c783251db45f8a8822a5
Gaalak <yoann.gralak@gmail.com>
2022-04-06 23:48:26 +0200

    ajout du projet

---
+++ Dictionary Replacer/src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+import org.junit.jupiter.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class MainTest {
+
+    @ParameterizedTest
+    @ValueSource
+
+}
