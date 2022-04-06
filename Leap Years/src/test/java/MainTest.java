ae2f481a395e7812939ace3a4ca40ee14358c573
Gaalak <yoann.gralak@gmail.com>
2022-04-06 23:28:31 +0200

    correction de la valeur à comparer

--- Leap Years/src/test/java/MainTest.java
+++ Leap Years/src/test/java/MainTest.java
@@ -11,21 +11,21 @@ public class MainTest {
     @ValueSource (ints = {2000})
     public void should_divided_by_400(int nb){
         String result = Main.anneeBissextile(nb);
-        assertEquals("Année bissextile",result);
+        assertEquals("L'année "+nb+" est bissextile",result);
     }
 
     @ParameterizedTest
     @ValueSource (ints = {1700, 1800, 1900})
     public void should_divided_by_100_and_not_400(int nb){
         String result = Main.anneeBissextile(nb);
-        assertEquals("Année non bissextile",result);
+        assertEquals("L'année "+nb+" n'est pas bissextile",result);
     }
 
     @ParameterizedTest
         @ValueSource (ints = {2008, 2012, 2016})
         public void should_divided_by_4_and_not_100(int nb){
             String result = Main.anneeBissextile(nb);
-            assertEquals("Année bissextile",result);
+            assertEquals("L'année "+nb+" est bissextile",result);
         }
 
 
@@ -33,7 +33,7 @@ public class MainTest {
         @ValueSource (ints = {2017, 2018, 2019})
         public void shouldnt_divided_by_4(int nb){
             String result = Main.anneeBissextile(nb);
-            assertEquals("Année non bissextile",result);
+            assertEquals("L'année "+nb+" n'est pas bissextile",result);
         }
 
 }
