46cf88bdcffcd4cce2326b12c1468ba9ecab89e4
Yoann <yoann.gralak@gmail.com>
2022-09-02 15:17:11 +0200

    lesson

---
+++ lesson/src/main/java/Voiture.java
@@ -0,0 +1,9 @@
+public class Voiture {
+
+    //propriétés, attributs, variables d'instance
+    int nbPortes = 5; //0 par féfaut
+    boolean automatique; //False par défaut
+    String couleur; // null par défaut
+    char premiereLettrePlaque; ///u0000 par défaut
+
+}
