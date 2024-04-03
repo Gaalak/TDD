cd77761cd9af661fc408d21c15b481ac66c54648
Yoann <yoann.gralak@gmail.com>
2024-04-03 15:18:35 +0200

    exercice gestion tableu

---
+++ Cours Simon/src/Listinteger.java
@@ -0,0 +1,23 @@
+public class Listinteger {
+    public class ListInteger {
+        int[] tab;
+
+        int get(int index){
+            index = tab.length -1;;
+            return index;
+        }
+
+        int insert(int value, int index){
+            index = tab[value];
+            return index;
+        }
+
+        int add(int number){
+            number = tab.length;
+            int[number] tab2;
+            tab.clone(tab2);
+            return tab;
+        }
+    }
+
+}
